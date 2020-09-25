ed928ed628cbe81c92eaa3a3d55f573c481eb590
Jim Schofield <jschofie@nerdery.com>
2020-09-25 16:33:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+#! /usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const MarkdownIt = require("markdown-it")();
+
+const __CWD = process.cwd();
+
+const args = process.argv.slice(2);
+
+if (args.length < 3) {
+  throw new Error(
+    "Not enough command line options.  Arguments must include source, destination, and templates folders in that order"
+  );
+}
+
+const [source, destination, templates] = args;
+const sourceDir = path.resolve(__CWD, source);
+const destinationDir = path.resolve(__CWD, destination);
+const templatesDir = path.resolve(__CWD, templates);
+
+// get file list
+const filesToRender = fs.readdirSync(sourceDir);
+console.log("Files to render:", filesToRender);
+
+// Make sure destination folder exists
+if (!fs.existsSync(destination)) {
+  fs.mkdirSync(destination);
+}
+
+function makeItem(dir, item) {
+  const file = item;
+  const slug = item.split(".md").join("");
+  const itemPath = path.resolve(dir, item);
+  const itemContent = fs.readFileSync(itemPath, "utf8");
+
+  return {
+    file,
+    slug,
+    itemPath,
+    itemContent,
+  };
+}
+
+function renderItem(itemContent) {
+  return MarkdownIt.render(itemContent);
+}
+
+function writeItem(content, slug, destinationPath) {
+  const newFile = slug + ".html";
+  fs.writeFileSync(path.resolve(destinationPath, newFile), content);
+}
+
+filesToRender.forEach((file) => {
+  // would be nice to have the pipeline operator :D
+  const item = makeItem(sourceDir, file);
+  const rendered = renderItem(item.itemContent);
+  writeItem(rendered, item.slug, destinationDir);
+});
